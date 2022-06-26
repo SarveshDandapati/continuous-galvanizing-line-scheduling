ea7c4ce6f66e90e2783ddcdc2d2be751bf2887ab
Sarvesh Dandapati <dsarvesh911@gmail.com>
2022-06-26 16:13:34 +0530

    ✨ Structure for order search section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import CreateSchedule from "./components/CreateSchedule";
+import OrderSearch from "./components/OrderSearch";
 import './css/create-schedule.css';
+import './css/order-search.css';
 function App() {
   return (
     <div className='wrapper'>
       <CreateSchedule/>
+      <OrderSearch/>
     </div>
   );
 }
---
+++ src/components/OrderSearch.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function orderSearch(){
+    return(
+        <form>
+            <input type='radio' id='ibm-order' name='order-type' value='ibm'/>
+            <label for='ibm-order'>IBM Order</label>
+            <input type='radio' id='sap-order' name='order-type' value='sap'/>
+            <label for='sap-order'>SAP Order</label> <br/> <br/>
+            <label for='order-id'>Order Item</label>
+            <input type='text' id='order-id' name='order-id' />
+        </form>
+    );
+}
+
+export default orderSearch;
